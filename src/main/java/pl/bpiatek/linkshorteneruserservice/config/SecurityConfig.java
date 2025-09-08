9181f890299d439131b21ec6c7dcecd7d7ba2b3f
Bartosz Piatek <your.email@example.com>
2025-09-08 14:00:06 +0200

    inject values when creating user

--- src/main/java/pl/bpiatek/linkshorteneruserservice/config/SecurityConfig.java
+++ src/main/java/pl/bpiatek/linkshorteneruserservice/config/SecurityConfig.java
@@ -1,15 +1,22 @@
 package pl.bpiatek.linkshorteneruserservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
@@ -17,6 +24,15 @@ import static org.springframework.security.config.http.SessionCreationPolicy.*;
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+
+    @Value("${monitoring.user.name}")
+    private String monitoringUsername;
+
+    @Value("${monitoring.user.password}")
+    private String monitoringPassword;
+
+    @Value("${monitoring.user.roles}")
+    private String[] monitoringRoles;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -26,6 +42,17 @@ class SecurityConfig {
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        var monitoringUser = User.builder()
+                .username(monitoringUsername)
+                .password(passwordEncoder().encode(monitoringPassword))
+                .roles(monitoringRoles)
+                .build();
+
+        return new InMemoryUserDetailsManager(monitoringUser);
     }
 
     @Bean
