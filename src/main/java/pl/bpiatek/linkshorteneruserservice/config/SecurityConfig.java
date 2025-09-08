b65aa0e02cc0e1b8b764642c6bd5506cffd70d76
Bartosz Piatek <your.email@example.com>
2025-09-08 16:34:23 +0200

    make prometheus endpoint available

--- src/main/java/pl/bpiatek/linkshorteneruserservice/config/SecurityConfig.java
+++ src/main/java/pl/bpiatek/linkshorteneruserservice/config/SecurityConfig.java
@@ -4,15 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -64,7 +61,8 @@ class SecurityConfig {
                         .requestMatchers(
                                 "/auth/register",
                                 "/auth/login",
-                                "/error").permitAll()
+                                "/error",
+                                "/actuator/prometheus").permitAll()
                         .requestMatchers("/actuator/**").authenticated()
                         .anyRequest().permitAll()
                 )
