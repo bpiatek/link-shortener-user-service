e561efb19600abd5f3b67d0e889bd023ef28aa89
Bartosz Piatek <your.email@example.com>
2025-09-10 21:20:31 +0200

    log stuff

--- src/main/java/pl/bpiatek/linkshorteneruserservice/user/JwtKeyProvider.java
+++ src/main/java/pl/bpiatek/linkshorteneruserservice/user/JwtKeyProvider.java
@@ -1,6 +1,8 @@
 package pl.bpiatek.linkshorteneruserservice.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +21,7 @@ import java.util.Map;
 @Component
 class JwtKeyProvider {
 
+    private static final Logger log = LoggerFactory.getLogger(JwtKeyProvider.class);
     private final KeyPair keyPair;
 
     JwtKeyProvider(@Value("${vault.secrets.path:/vault/secrets/jwtkeyjson}") String keyFilePath) {
@@ -42,6 +45,14 @@ class JwtKeyProvider {
             // 2. Parse the JSON into a Map
             var mapper = new ObjectMapper();
             Map<String, String> keys = mapper.readValue(jsonContent, Map.class);
+            log.info("Keys loaded from Vault: {}", keys);
+            if(keys.get("rsa-public-key") != null) {
+                log.info("RSA public key: {}", keys.get("rsa-public-key"));
+            }
+
+            if(keys.get("rsa-private-key") != null) {
+                log.info("RSA private key: {}", keys.get("rsa-private-key"));
+            }
 
             // 3. Decode the PEM strings into actual key objects
             var publicKey = decodePublicKey(keys.get("rsa-public-key"));
@@ -56,7 +67,7 @@ class JwtKeyProvider {
 
     // Helper method to parse the PEM-formatted private key
     private PrivateKey decodePrivateKey(String base64Key) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        var keyBytes = Base64.getDecoder().decode(base64Key);
+        var keyBytes = Base64.getDecoder().decode(base64Key.trim());
         var keySpec = new PKCS8EncodedKeySpec(keyBytes);
         var kf = KeyFactory.getInstance("RSA");
         return kf.generatePrivate(keySpec);
@@ -64,7 +75,7 @@ class JwtKeyProvider {
 
     // Helper method to decode a Base64 string into a PublicKey
     private PublicKey decodePublicKey(String base64Key) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        var keyBytes = Base64.getDecoder().decode(base64Key);
+        var keyBytes = Base64.getDecoder().decode(base64Key.trim());
         var keySpec = new X509EncodedKeySpec(keyBytes);
         var kf = KeyFactory.getInstance("RSA");
         return kf.generatePublic(keySpec);
