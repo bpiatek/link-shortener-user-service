e9d90c22cae0b02a2d35bfc57cdee427768f9ae6
Bartosz Piatek <your.email@example.com>
2025-09-11 14:33:21 +0200

    log headers in test-config endpoint

--- src/main/java/pl/bpiatek/linkshorteneruserservice/TestConfigController.java
+++ src/main/java/pl/bpiatek/linkshorteneruserservice/TestConfigController.java
@@ -1,11 +1,16 @@
 package pl.bpiatek.linkshorteneruserservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestConfigController {
+
+    private static final Logger log = LoggerFactory.getLogger(TestConfigController.class);
 
     private final String vaultTestMessage;
 
@@ -14,7 +19,15 @@ public class TestConfigController {
     }
 
     @GetMapping("/test-config")
-    public String getTestConfig() {
+    public String getTestConfig(
+            @RequestHeader(name = "X-User-Id", required = false) String userId,
+            @RequestHeader(name = "X-User-Role", required = false) String userRole
+    ) {
+
+        log.info("Request received for /test-config");
+        log.info("Header X-User-Id: {}", userId);
+        log.info("Header X-User-Role: {}", userRole);
+
         return "The message from Vault is: '" + this.vaultTestMessage + "'";
     }
 }
